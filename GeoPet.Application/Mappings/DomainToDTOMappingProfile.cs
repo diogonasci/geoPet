f0d12b80e70ffb11179cb9ba08ab9c2fff159394
diogonasci <diogonasci@hotmail.com>
2022-12-16 13:31:17 -0300

    Implementa serviços

---
+++ GeoPet.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using GeoPet.Application.DTOs;
+using GeoPet.Domain.Entities;
+
+namespace GeoPet.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<PetOwner, PetOwnerDTO>().ReverseMap();
+            CreateMap<Pet, PetDTO>().ReverseMap();
+        }
+    }
+}
---
+++ GeoPet.Application/Services/PetOwnerService.cs
@@ -0,0 +1,50 @@
+using AutoMapper;
+using GeoPet.Application.DTOs;
+using GeoPet.Application.Interfaces;
+using GeoPet.Domain.Entities;
+using GeoPet.Domain.Interfaces;
+
+namespace GeoPet.Application.Services
+{
+    public class PetOwnerService : IPetOwnerService
+    {
+        private IPetOwnerRepository _petOwnerRepository;
+        private readonly IMapper _mapper;
+        public PetOwnerService(IPetOwnerRepository petOwnerRepository,
+            IMapper mapper)
+        {
+            _petOwnerRepository = petOwnerRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<PetOwnerDTO>> GetPetOwners()
+        {
+            var categoriesEntity = await _petOwnerRepository.GetPetOwnersAsync();
+            return _mapper.Map<IEnumerable<PetOwnerDTO>>(categoriesEntity);
+        }
+
+        public async Task<PetOwnerDTO> GetById(int? id)
+        {
+            var petOwnerEntity = await _petOwnerRepository.GetByIdAsync(id);
+            return _mapper.Map<PetOwnerDTO>(petOwnerEntity);
+        }
+
+        public async Task Add(PetOwnerDTO petOwnerDto)
+        {
+            var petOwnerEntity = _mapper.Map<PetOwner>(petOwnerDto);
+            await _petOwnerRepository.CreateAsync(petOwnerEntity);
+        }
+
+        public async Task Update(PetOwnerDTO petOwnerDto)
+        {
+            var petOwnerEntity = _mapper.Map<PetOwner>(petOwnerDto);
+            await _petOwnerRepository.UpdateAsync(petOwnerEntity);
+        }
+
+        public async Task Remove(int? id)
+        {
+            var petOwnerEntity = _petOwnerRepository.GetByIdAsync(id).Result;
+            await _petOwnerRepository.RemoveAsync(petOwnerEntity);
+        }
+    }
+}
